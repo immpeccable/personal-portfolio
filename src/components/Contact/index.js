c9bc44cde711366e7575e2580d5386be46177577
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-07-11 12:29:01 +0300

    contact functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,35 +1,78 @@
 import { useState } from "react";
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 let Contact = () => {
+  const form = useRef();
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    function handleSubmit(e){
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        if(name === ""){
-            return;
-        }
-        if(email === ""){
-            return;
-        }
-        if(message === ""){
-            return;
-        }
-        
+    if (name === "") {
+      alert("Contact name can not be empty!");
+      return;
+    }
+    if (email === "") {
+      alert("Contact email can not be empty!");
+      return;
+    }
+    if (message === "") {
+      alert("Contact message can not be empty!");
+      return;
     }
 
+    emailjs
+      .sendForm(
+        'service_31ujhzh',
+        'template_ejxqgpt',
+        form.current,
+        '4du8p9Hw8xtZkq3py'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
   return (
     <div className="contact-wrapper">
       <h2 className="contact-title">CONTACT</h2>
 
-      <input onChange={(e)  => {setName(e.target.value)}} className="name-input" placeholder="Your name..."></input>
-      <input onChange={(e)  => {setEmail(e.target.value)}} className="email-input" placeholder="Your e-mail..."></input>
-      <textarea onChange={(e)  => {setMessage(e.target.value)}} className="message-input" placeholder="Your message..."></textarea>
-      <button onClick={(e) => handleSubmit(e)} className="contact-button">GET IN TOUCH</button>
+      <form className="form-wrapper" ref={form} onSubmit={handleSubmit}>
+        <input type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className="name-input"
+          placeholder="Your name..."
+        ></input>
+        <input type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          className="email-input"
+          placeholder="Your e-mail..."
+        ></input>
+        <textarea
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          className="message-input"
+          placeholder="Your message..."
+        ></textarea>
+        <input type="submit" className="contact-button" value="GET IN TOUCH">
+          
+        </input>
+      </form>
     </div>
   );
 };
